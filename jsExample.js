6c02c4733cd07f6cf1006e11a5cd22acbe095042
DenisM002 <X00170716@myTUDublin.ie>
2021-07-26 11:47:39 +0100

    Practise simple string + Object manipulation

--- jsExample.js
+++ jsExample.js
@@ -16,5 +16,32 @@
 
 // console.log( typeof name1);
 
-let val= Math.floor(Math.random() * 100);
-console.log(val);
+
+// let val= Math.floor(Math.random() * 100);
+// console.log(val);
+
+let val;
+
+// Escaping: Use \ before an inline quote to allow it be part of the string. Causes a quote to become a reg character
+val = 'That\'s great how are you doing m\'babe';
+
+// Property vs Method
+
+
+// Creating Object and diff between Methods + Properties
+// let dog = {};
+
+// dog.isSmelly = false;
+
+// console.log(dog.isSmelly);
+
+
+// dog.makeSmelly = function() {
+//   dog.isSmelly = true;
+// }
+
+// dog.makeSmelly();
+
+
+// console.log(dog.isSmelly);
+
