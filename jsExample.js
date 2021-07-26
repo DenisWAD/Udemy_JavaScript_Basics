d898399ab3fac89525634da8821c8c6eda7393ea
DenisM002 <X00170716@myTUDublin.ie>
2021-07-26 11:50:02 +0100

    New file setup for individual lessons

--- jsExample.js
+++ jsExample.js
@@ -1,47 +1,22 @@
 
-// let name1 ="Denis Murray";
-// console.log(name1);
+let name1 ="Denis Murray";
+console.log(name1);
 
-// const person = {
-//   name: 'Denis',
-//   age: 26
-// };
+const person = {
+  name: 'Denis',
+  age: 26
+};
 
-// console.log(person);
+console.log(person);
 
-// person.age = 30;
-// console.log(person);
+person.age = 30;
+console.log(person);
 
-// console.log( typeof person);
+console.log( typeof person);
 
-// console.log( typeof name1);
-
-
-// let val= Math.floor(Math.random() * 100);
-// console.log(val);
-
-let val;
-
-// Escaping: Use \ before an inline quote to allow it be part of the string. Causes a quote to become a reg character
-val = 'That\'s great how are you doing m\'babe';
-
-// Property vs Method
+console.log( typeof name1);
 
 
-// Creating Object and diff between Methods + Properties
-// let dog = {};
-
-// dog.isSmelly = false;
-
-// console.log(dog.isSmelly);
-
-
-// dog.makeSmelly = function() {
-//   dog.isSmelly = true;
-// }
-
-// dog.makeSmelly();
-
-
-// console.log(dog.isSmelly);
+let val= Math.floor(Math.random() * 100);
+console.log(val);
 
